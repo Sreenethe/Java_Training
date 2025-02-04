b863a24cabd7b03c1ad9324b4608b3cf67627c9a
Sreenethe T S <sreenethe13@gmil.com>
2025-02-04 15:42:23 +0530

    Placement Training

--- Day_2/Grade.java
+++ Day_2/Grade.java
@@ -4,17 +4,24 @@ public class Grade {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your Marks to get Grade:");
         int mark = scan.nextInt();
-        switch (key:var) {
-            case (mark>=90 || mark<=100 ):
-                System.out.println("Your Grade is A");
-                break;
-            case (mark>=80 || mark<=89):
-                System.out.println("Your grade is B");
-                break;
-            case (mark>):
         
-            default:
-                break;
-        }
+    switch (mark) {
+        case 1:
+            System.out.println("Your Grade is A");
+            break;
+        case 2:
+            System.out.println("Your grade is B");
+            break;
+        case 3:
+            System.out.println("Your grade is C");
+            break;
+        case 4:
+            System.out.println("Your grade is D");
+    
+        default:
+            System.out.println("You have no grade");
+            break;
+    }
+        
     }
 }
