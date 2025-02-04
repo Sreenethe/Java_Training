81d5ab2abd31ee4f468c776bd73a64888f9c8316
Sreenethe T S <sreenethe13@gmil.com>
2025-02-04 11:25:58 +0530

    Training in progress

---
+++ Day_2/Grade.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Grade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your Marks to get Grade:");
+        int mark = scan.nextInt();
+        switch (key:var) {
+            case (mark>=90 || mark<=100 ):
+                System.out.println("Your Grade is A");
+                break;
+            case (mark>=80 || mark<=89):
+                System.out.println("Your grade is B");
+                break;
+            case (mark>):
+        
+            default:
+                break;
+        }
+    }
+}
