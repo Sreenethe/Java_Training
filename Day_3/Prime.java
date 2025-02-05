3e83265521352fd8997e9aa91848b980d55659f9
Sreenethe T S <sreenethe13@gmil.com>
2025-02-05 12:04:20 +0530

    Training

---
+++ Day_3/Prime.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n:");
+        int n = sc.nextInt();
+        boolean isprime = true;
+        if(n<=1){
+            System.out.println("Is not a prime");
+        }
+        else{
+            for(i=0;)
+        }
+        
+        
+    }
+    
+}
