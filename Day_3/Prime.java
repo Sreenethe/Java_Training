52bbd0b59141c9ad3d4b788e471e4b96e1531f23
Sreenethe T S <sreenethe13@gmil.com>
2025-02-06 10:01:50 +0530

    added

--- Day_3/Prime.java
+++ Day_3/Prime.java
@@ -1,15 +1,22 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 public class Prime {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter n:");
-        int n = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter n:");
+        // int n = sc.nextInt();
+        int n = 11;
+        int div = 2;
         boolean isprime = true;
-        if(n<=1){
-            System.out.println("Is not a prime");
+        while(div<=n/2){
+            if(n%div==0){
+                isprime = false;
+            }
+        }
+        if(isprime){
+            System.out.println("Prime number");
         }
         else{
-            for(i=0;)
+            System.out.println("Not a prime no");
         }
         
         
